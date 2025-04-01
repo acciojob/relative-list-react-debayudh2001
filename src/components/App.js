e4a447ad01c19b3254e3e7d3ab94eab4a9ea77a5
debayudh2001 <debayudh31@gmail.com>
2025-04-01 19:35:57 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  //const [relatives, setRelatives] = useState(["relative1","relative2","relative3","relative4","relative5"])
+  let relatives = ["relative1","relative2","relative3","relative4","relative5"]
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol key={"relativeList"}>
+        {relatives.map((relative, ind) => (
+          <li key={`relativeListItem${ind+1}`}>{relative}</li>
+        ))}
+      </ol>
     </div>
   )
 }
